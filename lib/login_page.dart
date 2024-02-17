571a968634b5ddb34589f180dc63f08dcbe8f78a
Pranav M R <148135964+pranav-m-r@users.noreply.github.com>
2024-02-17 20:16:54 +0530

    Sign Up New Users

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -23,72 +23,6 @@ class AuthPage extends StatelessWidget {
   }
 }
 
-class MyTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-  final bool obscureText;
-
-  const MyTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.obscureText,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        decoration: InputDecoration(
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
-            hintStyle: TextStyle(color: Colors.grey[500])),
-      ),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  final Function()? onTap;
-
-  const MyButton({super.key, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: const Center(
-          child: Text(
-            "Sign In",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -123,12 +57,24 @@ class _LoginPageState extends State<LoginPage> {
     }
     on FirebaseAuthException catch (e) {
       if (mounted) {Navigator.pop(context);}
-      if (e.code == 'user-not-found') {
-        showErrorMessage('Incorrect Email');
-      }
-      else if (e.code == 'wrong-password') {
-        showErrorMessage('Incorrect Password');
-      }
+      showErrorMessage(e.code);
+    }
+  }
+
+  void signUserUp() async {
+    showDialog(context: context, builder: (context) {
+      return const Center(child: CircularProgressIndicator());
+    });
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    if (mounted) {Navigator.pop(context);}
+    }
+    on FirebaseAuthException catch (e) {
+      if (mounted) {Navigator.pop(context);}
+      showErrorMessage(e.code);
     }
   }
 
@@ -143,68 +89,87 @@ class _LoginPageState extends State<LoginPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 50),
-                const Icon(Icons.lock,size: 100,),
+                const Icon(Icons.lock,size: 60,),
                 const SizedBox(height: 50),
             
                 Text(
-                  'Welcome back you\'ve been missed!',
+                  'Welcome!',
                   style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
+                    color: Colors.grey[900],
+                    fontSize: 25,
                   ),
                 ),
                 const SizedBox(height: 25),
-            
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  obscureText: false,
+
+                Text(
+                  'Sign in or sign up with your details!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 18,
+                  ),
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 40),
             
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                          controller: emailController,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey.shade400),
+                              ),
+                              fillColor: Colors.grey.shade200,
+                              filled: true,
+                              hintText: 'Email',
+                              hintStyle: TextStyle(color: Colors.grey[500])),
+                        ),
                 ),
                 const SizedBox(height: 10),
             
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 25),
-            
-                MyButton(
-                  onTap: signUserIn,
+                  child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey.shade400),
+                              ),
+                              fillColor: Colors.grey.shade200,
+                              filled: true,
+                              hintText: 'Password',
+                              hintStyle: TextStyle(color: Colors.grey[500])),
+                        ),
                 ),
                 const SizedBox(height: 50),
             
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Not a member?',
-                      style: TextStyle(color: Colors.grey[700]),
-                    ),
-                    const SizedBox(width: 4),
-                    const Text(
-                      'Register now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                )
+                GestureDetector(
+                  onTap: signUserIn,
+                  child: Container(
+                    padding: const EdgeInsets.all(25),
+                    margin: const EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8),),
+                    child: const Center(child: Text("Sign In",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16,),
+                    ),),),),
+                const SizedBox(height: 20),
+
+                GestureDetector(
+                  onTap: signUserUp,
+                  child: Container(
+                    padding: const EdgeInsets.all(25),
+                    margin: const EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8),),
+                    child: const Center(child: Text("Sign Up",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16,),
+                    ),),),),
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'login_page.dart';
 
@@ -51,12 +52,12 @@ class _MyHomePageState extends State<MyHomePage> {
   double num1 = 0.0;
   double num2 = 0.0;
   double temp = 0.0;
-  bool justcalc = false;
+  bool shouldClear = false;
 
-  bool darkTheme = false;
-  Color textboxColor = Colors.white;
+  bool darkEnabled = false;
+  Color textBoxColor = Colors.white;
   Color backgroundColor = Colors.white;
-  Color appbarColor = Colors.deepOrange.shade800;
+  Color appBarColor = Colors.deepOrange.shade800;
   IconData themeIcon = Icons.dark_mode;
 
   var style1 = ElevatedButton.styleFrom(backgroundColor: Colors.red.shade500, foregroundColor: Colors.white,
@@ -66,29 +67,35 @@ class _MyHomePageState extends State<MyHomePage> {
   var style3 = ElevatedButton.styleFrom(backgroundColor: Colors.deepOrange.shade300, foregroundColor: Colors.white,
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),);
 
-  void _backspace(){
+  void addUserHistory() async {
+    await FirebaseFirestore.instance.collection('users').add({
+      
+    });
+  }
+
+  void backspace(){
     setState(() {
-      if (justcalc == true) {
-        _clear();
+      if (shouldClear == true) {
+        clear();
       }
       ans = ans.substring(0, ans.length - 1);
     });
   }
 
-  void _clear() {
+  void clear() {
     setState(() {
       ans = '';
       prev = '';
       oper = '';
       num1 = 0.0;
       num2 = 0.0;
-      justcalc = false;
+      shouldClear = false;
     });
   }
 
-  void _calculate() {
+  void calculate() {
     setState(() {
-      justcalc = true;
+      shouldClear = true;
       if (ans.isNotEmpty) {
         num2 = double.parse(ans);
         if (oper == '+') {
@@ -114,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
         }
         else if (oper == '÷') {
           if (num2 == 0) {
-            _clear();
+            clear();
           }
           else {
             ans = (num1 / num2).toString();
@@ -124,18 +131,18 @@ class _MyHomePageState extends State<MyHomePage> {
             oper = '';
           }
         }
-        _setprecision();
+        setPrecision();
       }
       else {
-        justcalc = false;
+        shouldClear = false;
       }
     });
   }
 
-  void _numClicked(numC){
+  void numClicked(numC){
     setState(() {
-      if (justcalc == true) {
-        _clear();
+      if (shouldClear == true) {
+        clear();
       }
       if (numC=='.'){
         if (!ans.contains('.')){
@@ -148,14 +155,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _operclicked(String ope){
+  void operClicked(String ope){
     setState(() {
-      justcalc = false;
+      shouldClear = false;
       if (ans.isNotEmpty) {
         prev = ans;
         if (oper != '') {
-          _calculate();
-          justcalc = false;
+          calculate();
+          shouldClear = false;
           prev = ans;
           num1 = double.parse(ans);
           ans = '';
@@ -173,59 +180,59 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _changesign(){
+  void changeSign(){
     setState(() {
       temp = double.parse(ans) * (-1.0);
       ans = temp.toString();
-      _setprecision();
-      justcalc == false;
+      setPrecision();
+      shouldClear == false;
     });
   }
 
-  void _percentage(){
+  void percentage(){
     setState(() {
       temp = double.parse(ans) / 100.0;
       ans = temp.toString();
-      _setprecision();
-      justcalc == true;
+      setPrecision();
+      shouldClear == true;
     });
   }
 
-  void _constants(k){
+  void constants(k){
     setState(() {
-      if (justcalc == true) {
-        _clear();
+      if (shouldClear == true) {
+        clear();
       }
       if (k == 'π') {
         ans = '3.14159';
-        justcalc == false;
+        shouldClear == false;
       }
       else if (k == 'e') {
         ans = '2.71828';
-        justcalc == false;
+        shouldClear == false;
       }
     });
   }
 
-  void _square() {
+  void square() {
     setState(() {
       temp = double.parse(ans);
       ans = (temp * temp).toString();
-      _setprecision();
-      justcalc == true;
+      setPrecision();
+      shouldClear == true;
     });
   }
 
-  void _cube() {
+  void cube() {
     setState(() {
       temp = double.parse(ans);
       ans = (temp * temp * temp).toString();
-      _setprecision();
-      justcalc == true;
+      setPrecision();
+      shouldClear == true;
     });
   }
 
-  void _setprecision(){
+  void setPrecision(){
     setState(() {
       temp = double.parse(ans);
       if ((temp - (temp.round()).toDouble()).abs() < 1e-12) {
@@ -240,13 +247,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _darkTheme() {
+  void darkTheme() {
     setState(() {
-      if (!darkTheme) {
+      if (!darkEnabled) {
         backgroundColor = Colors.black;
-        darkTheme = true;
-        textboxColor = Colors.white70;
-        appbarColor = Colors.black;
+        darkEnabled = true;
+        textBoxColor = Colors.white70;
+        appBarColor = Colors.black;
         themeIcon = Icons.light_mode;
         style1 = ElevatedButton.styleFrom(
           backgroundColor: Colors.cyan.shade900, foregroundColor: Colors.white,
@@ -265,9 +272,9 @@ class _MyHomePageState extends State<MyHomePage> {
       }
       else {
         backgroundColor = Colors.white;
-        darkTheme = false;
-        textboxColor = Colors.white;
-        appbarColor = Colors.deepOrange.shade800;
+        darkEnabled = false;
+        textBoxColor = Colors.white;
+        appBarColor = Colors.deepOrange.shade800;
         themeIcon = Icons.dark_mode;
         style1 = ElevatedButton.styleFrom(
           backgroundColor: Colors.red.shade500, foregroundColor: Colors.white,
@@ -287,7 +294,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _signout() {
+  void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
@@ -304,21 +311,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar( foregroundColor: Colors.white,
-        backgroundColor: appbarColor,
+        backgroundColor: appBarColor,
         title: Text(widget.title),centerTitle: true,
         actions: <Widget>[
           IconButton(
             icon: Icon(themeIcon),
             onPressed: () {
               setState(() {
-                _darkTheme();
+                darkTheme();
               });
             },),
             IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () {
               setState(() {
-                _signout();
+                signUserOut();
               });
             },)
             ],
@@ -330,65 +337,65 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           SizedBox(width: screenWidth*0.975, height: screenHeight*0.07, child: OutlinedButton(style: TextButton.styleFrom
-            (shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), backgroundColor: textboxColor), onPressed: null,
+            (shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), backgroundColor: textBoxColor), onPressed: null,
             child: Text(prev, style: Theme.of(context).textTheme.headlineSmall,),),),
           SizedBox(width: screenWidth*0.975, height: screenHeight*0.07, child: OutlinedButton(style: TextButton.styleFrom
-            (shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), backgroundColor: textboxColor), onPressed: null,
+            (shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), backgroundColor: textBoxColor), onPressed: null,
             child: Text(ans, style: Theme.of(context).textTheme.headlineSmall,),),),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style1, _clear, 'C'),
-                  uiButton(0.18, 0.085, style2, _square, 'x²'),
-                  uiButton(0.18, 0.085, style2, _cube, 'x³'),
-                  uiButton(0.18, 0.085, style1, _backspace, '⌫'),
+                  uiButton(0.18, 0.085, style1, clear, 'C'),
+                  uiButton(0.18, 0.085, style2, square, 'x²'),
+                  uiButton(0.18, 0.085, style2, cube, 'x³'),
+                  uiButton(0.18, 0.085, style1, backspace, '⌫'),
                 ],
               ),
               SizedBox(height: screenHeight*0.02),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style2, _percentage, '%'),
-                  uiButton(0.18, 0.085, style2, (){_constants('e');}, 'e'),
-                  uiButton(0.18, 0.085, style2, (){_constants('π');}, 'π'),
-                  uiButton(0.18, 0.085, style2, (){_operclicked('+');}, '+'),
+                  uiButton(0.18, 0.085, style2, percentage, '%'),
+                  uiButton(0.18, 0.085, style2, (){constants('e');}, 'e'),
+                  uiButton(0.18, 0.085, style2, (){constants('π');}, 'π'),
+                  uiButton(0.18, 0.085, style2, (){operClicked('+');}, '+'),
                 ],
               ),
               SizedBox(height: screenHeight*0.02),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style3, (){_numClicked('7');}, '7'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('8');}, '8'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('9');}, '9'),
-                  uiButton(0.18, 0.085, style2, (){_operclicked('-');}, '-'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('7');}, '7'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('8');}, '8'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('9');}, '9'),
+                  uiButton(0.18, 0.085, style2, (){operClicked('-');}, '-'),
                 ],
               ),
               SizedBox(height: screenHeight*0.02),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style3, (){_numClicked('4');}, '4'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('5');}, '5'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('6');}, '6'),
-                  uiButton(0.18, 0.085, style2, (){_operclicked('x');}, 'x'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('4');}, '4'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('5');}, '5'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('6');}, '6'),
+                  uiButton(0.18, 0.085, style2, (){operClicked('x');}, 'x'),
                 ],
               ),
               SizedBox(height: screenHeight*0.02),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style3, (){_numClicked('1');}, '1'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('2');}, '2'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('3');}, '3'),
-                  uiButton(0.18, 0.085, style2, (){_operclicked('÷');}, '÷'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('1');}, '1'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('2');}, '2'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('3');}, '3'),
+                  uiButton(0.18, 0.085, style2, (){operClicked('÷');}, '÷'),
                 ],
               ),
               SizedBox(height: screenHeight*0.02),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  uiButton(0.18, 0.085, style2, _changesign, '±'),
-                  uiButton(0.18, 0.085, style3, (){_numClicked('0');}, '0'),
-                  uiButton(0.18, 0.085, style2, (){_numClicked('.');}, '.'),
-                  uiButton(0.18, 0.085, style1, _calculate, '='),
+                  uiButton(0.18, 0.085, style2, changeSign, '±'),
+                  uiButton(0.18, 0.085, style3, (){numClicked('0');}, '0'),
+                  uiButton(0.18, 0.085, style2, (){numClicked('.');}, '.'),
+                  uiButton(0.18, 0.085, style1, calculate, '='),
                 ],
               ),
             ],),
